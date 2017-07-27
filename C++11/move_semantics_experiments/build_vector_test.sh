11674f2d8523a6bcbb1bedf7d600aab00e879adc
gw111zz <gw111zz@gmail.com>
2017-07-27 18:02:43 +0100

    Add results of vector of move / copy profiling.

--- C++11/move_semantics_experiments/build_vector_test.sh
+++ C++11/move_semantics_experiments/build_vector_test.sh
@@ -6,3 +6,14 @@ echo "time ./vector_of_move_only_object"
 echo "And then:"
 echo "time ./vector_of_copy_only_object"
 
+# time ./vector_of_move_only_object
+# 
+# real	5m30.799s
+# user	2m40.973s
+# sys	1m55.757s
+# 
+# time ./vector_of_copy_only_object
+# 
+# real	6m5.041s
+# user	3m10.117s
+# sys	1m53.882s
