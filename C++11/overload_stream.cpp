765a0f8c834f8f04e478ae663a1b4f03d9157109
gw111zz <gw111zz@gmail.com>
2018-01-19 11:27:15 +0000

    Friend function test

--- C++11/overload_stream.cpp
+++ C++11/overload_stream.cpp
@@ -8,15 +8,21 @@ public:
     Person(std::string name, int age) : mName(std::move(name)), mAge(age) {
     }
 
-    friend std::ostream& operator<<(std::ostream& stream, const Person& person) {
+    /*friend std::ostream& operator<<(std::ostream& stream, const Person& person) {
         return stream << "Person(name:" << person.mName << ", age:" << person.mAge << ")";
-    }
+    }*/
+
+    friend std::ostream& operator<<(std::ostream& stream, const Person& person);
 
 private:
 
     std::string mName;
     int mAge;
 };
+
+std::ostream& operator<<(std::ostream& stream, const Person& person) {
+    return stream << "Person(name:" << person.mName << ", age:" << person.mAge << ")";
+}
 
 int main() {
 
